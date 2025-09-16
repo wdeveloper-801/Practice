84413ea659a7375569a1b620db2b431b39ae21bb
wdeveloper-801 <wdeveloper149@gmail.com>
2025-09-16 20:36:43 +0500

    Add files via upload

---
+++ again learning webdevelopment/javasciprt/javascript loops part2.js
@@ -0,0 +1,42 @@
+// in object we use forin loop. and in array we use for of loop..
+
+//arrey exapmle for of loop
+// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (const element of array) {
+//     console.log(element);
+// }
+
+//example
+// let gra = "helloworld"
+// for (const element of gra) {
+//     // console.log(`${element} ${gra}`);
+// }
+
+
+
+//new foreach
+// let programings = ['jsx', 'python', 'java', 'c++', 'c', ]
+// programings.forEach((item) => {
+//     console.log(item);
+
+// })
+// for (const element of programings) {
+//     console.log(element);
+
+// }
+
+
+
+let pro = [
+    {
+        programing: "jsx",
+        programings: "python",
+    },
+
+]
+pro.forEach((item,) => {
+    console.log(item.programing);
+    console.log(item.programings);
+
+});
+
